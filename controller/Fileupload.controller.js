008980d96c136535739d055d702bb7ba53d84c35
deepakmeena78 <deepakmeenaa78@gmail.com>
2025-08-20 15:47:24 +0530

    new

--- controller/Fileupload.controller.js
+++ controller/Fileupload.controller.js
@@ -1,54 +1,6 @@
 import fs from "fs";
 import { v2 as cloudinary } from "cloudinary";
 
-// export const uploadVideo = async (req, res) => {
-//   try {
-//     if (!req.files || !req.files.video) {
-//       return res.status(400).json({ message: "No video uploaded" });
-//     }
-
-//     const videoFile = req.files.video;
-
-//     if (!videoFile.mimetype.startsWith("video/")) {
-//       return res.status(400).json({ message: "Only video files are allowed" });
-//     }
-
-//     if (videoFile.size < 20 * 1024 * 1024 || videoFile.size > 100 * 1024 * 1024) {
-//       return res.status(400).json({ message: "File must be 20–100 MB" });
-//     }
-
-//     const stream = cloudinary.uploader.upload_stream(
-//       {
-//         resource_type: "video",
-//         folder: "uploaded_videos",
-//       },
-//       (err, result) => {
-//         if (err) {
-//           console.error("Cloudinary error:", err);
-//           return res.status(500).json({ message: "Upload failed", error: err.message });
-//         }
-
-//         if (videoFile.tempFilePath && fs.existsSync(videoFile.tempFilePath)) {
-//           fs.unlinkSync(videoFile.tempFilePath);
-//         }
-
-//         return res.json({
-//           url: result.secure_url,
-//           public_id: result.public_id,
-//           created_at: result.created_at,
-//           format: result.format,
-//           duration: result.duration,
-//         });
-//       }
-//     );
-
-//     fs.createReadStream(videoFile.tempFilePath).pipe(stream);
-//   } catch (err) {
-//     console.error("Upload error:", err);
-//     res.status(500).json({ message: "Upload failed", error: err.message });
-//   }
-// };
-
 
 export const uploadVideo = async (req, res) => {
   try {
